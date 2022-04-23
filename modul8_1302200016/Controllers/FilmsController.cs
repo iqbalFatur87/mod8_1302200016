648af71130aa4663ed0aa9e5f5ccbd89da043737
MUHAMAD IQBAL FATUROHMAN <iqbalfatur@365.telkomuniversity.ac.id>
2022-04-23 16:58:21 +0700

    menambahkan API Movies

---
+++ modul8_1302200016/Controllers/FilmsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul8_1302200016.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Film>> GetFilms()
+        {
+           return Film.Films.ToList();
+        }
+
+		[HttpPost]
+		public ActionResult<Film> TambahFilm(Film film)
+		{
+			Film.Films.Add(film);
+			return Ok("Film telah ditambahkan");
+		}
+
+		[HttpGet]
+		[Route("{id}")]
+		public ActionResult<Film> GetFilm(int id)
+		{
+			Film film = Film.Films[id];
+			return Ok(film);
+		}
+
+		[HttpDelete]
+		[Route("{id}")]
+		public ActionResult DeleteMovie(int id)
+		{
+			Film.Films.Remove(Film.Films[id]);
+			return Ok("Film Sukses terhapus");
+		}
+	}
+}
--- modul8_1302200016/Film.cs
+++ modul8_1302200016/Film.cs
@@ -2,6 +2,22 @@
 {
     public class Film
     {
+        // Database
+        public static List<Film> Films = new List<Film>()
+        {
+            new Film() { Title = "The Shawshank Redemption", Director = "Frank Darabont" , Descriptions = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", Stars = {"Tim R.", "Morgan F.", "Bob Gunton" } },
+            new Film() { Title = "The GodFather", Director = "Francis Ford Coppola" , Descriptions = "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.", Stars = {"Marlon B.", "Al Pacino", "James Caan"} },
+            new Film() { Title = "The Dark Knight", Director = "Christopher Nolan" , Descriptions = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", Stars = {"Christian B.", "Heath L.", "Aaron E."} }
+        };
 
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Descriptions { get; set; }
+
+        public Film()
+        {
+            
+        }
     }
 }
